0106f7c76bdc1b3b847ec29eddd3e72b1cdba6cc
frontendprof <abdumaliksharipov@gmail.com>
2020-09-07 23:48:19 +0500

    Displaying forms

---
+++ testing3/src/components/App.js
@@ -0,0 +1,17 @@
+
+
+import React from "react";
+import SearchBar from "./SearchBar";
+
+
+const App=()=>{
+    return(
+
+        <div>
+            <SearchBar />
+        </div>
+    )
+        
+};
+
+export default App;
---
+++ testing3/src/components/SearchBar.js
@@ -0,0 +1,21 @@
+
+
+import React from "react";
+
+
+
+class SearchBar extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <form>
+                    <input type="text"/>
+                </form>
+            </div>
+        )
+    }
+}
+
+
+export default SearchBar;
