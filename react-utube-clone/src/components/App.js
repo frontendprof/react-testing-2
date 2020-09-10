6d6de3116ce56b7fbf3eea1504f4273cc1e9e781
frontendprof <abdumaliksharipov@gmail.com>
2020-09-10 11:58:05 +0500

    Accessing utube api and putting all together

--- react-utube-clone/src/components/App.js
+++ react-utube-clone/src/components/App.js
@@ -1,13 +1,31 @@
 
 import React from "react";
 
+import SearchBar from "./SearchBar";
+import youtube from "../apis/utube";
+import utube from "../apis/utube";
+
 
 class App extends React.Component{
 
 
+    onTermSubmit=e=>{
+        
+        utube.get('/search',{
+            params:{
+                q:e
+            }
+        })
+    }
+
+
     render(){
 
-        return <div>App!</div>;
+        return (
+        <div className="ui container">
+            <SearchBar onFormSubmit={this.onTermSubmit}/>
+        </div>
+        );
 
     }
 }
