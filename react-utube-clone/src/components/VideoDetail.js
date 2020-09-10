8e90fe76f96eb88f50a64f9a070db55504f34d7a
frontendprof <abdumaliksharipov@gmail.com>
2020-09-10 22:38:49 +0500

    Displaying a video player

--- react-utube-clone/src/components/VideoDetail.js
+++ react-utube-clone/src/components/VideoDetail.js
@@ -8,13 +8,26 @@ const VideoDetail=({video})=>{
     if(!video){
         return <h4>Loading ...</h4>
     }
+    
+    const videoSrc=`https://www.youtube.com/embed/${video.id.videoId}`;
+
     return (
+        <>
+
+            <div className="ui embed">
+                <iframe src={videoSrc} ></iframe>
+            </div>
         
-        <div className="ui segment">
-            <h4 className="ui header">{video.snippet.title}</h4>
-            <p>{video.snippet.description}</p>
+            <div className="ui segment">
+                <h4 className="ui header">{video.snippet.title}</h4>
+                <p>{video.snippet.description}</p>
             
-        </div>
+            </div>
+
+
+        </>
+
+        
     )
 }
 
