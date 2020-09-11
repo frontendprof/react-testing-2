347917c8598bf185e1f37889ed084d8150fab2fa
frontendprof <abdumaliksharipov@gmail.com>
2020-09-11 12:41:32 +0500

    Helper functions in func components

--- react-widget/src/App.js
+++ react-widget/src/App.js
@@ -1,10 +1,31 @@
 
 import React from 'react';
 
+import Accordion from "./components/Accordion";
 
+
+const items=[
+    {
+        title:"What is React",
+        content:'React is front end javascript library for building user interfaces'
+    },
+    {
+        title:"What is the difference between React and Angular",
+        content:"Angular JS is based on MVC (Model View Controller) whereas React is based on Virtual DOM."
+
+    },
+    {
+        title:"How do you use React",
+        content:"You use React by creating components"
+    }
+]
 
 
 export default ()=>{
-    return <h1>Widgets App</h1>
+    return (
+    <h3>
+        <Accordion items={items} />
+    </h3>
+    );
 };
 
