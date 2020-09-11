72e861a6ad53dbc0222212025fe11a612cde83f6
frontendprof <abdumaliksharipov@gmail.com>
2020-09-11 23:51:08 +0500

    List building

--- react-widget/src/components/Search.js
+++ react-widget/src/components/Search.js
@@ -35,6 +35,20 @@ const Search=()=>{
 
     },[term]);
 
+    
+    const renderedResults=results.map((result)=>{
+        return(
+            <div key={result.pageid} className="item">
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    {result.snippet}
+                </div>
+            </div>
+        )
+    })
+
     return (
 
         <div>
@@ -47,6 +61,7 @@ const Search=()=>{
                     className="input"/>
                 </div>
             </div>
+            <div className="ui celled list">{renderedResults}</div>
         </div>
     )
 };
