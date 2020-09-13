a3d38992100b150c99eef7795b169f0476fe7bc9
frontendprof <abdumaliksharipov@gmail.com>
2020-09-13 22:52:33 +0500

    Disebouncing translation updates

--- react-widget/src/components/Convert.js
+++ react-widget/src/components/Convert.js
@@ -8,6 +8,19 @@ import axios from "axios";
 const Convert=({language,text})=>{
 
     const [translated,setTranslated]=useState("");
+    const [debouncedText,setDebouncedText]=useState(text);
+
+    useEffect(()=>{
+
+        const timerId=setTimeout(()=>{
+            setDebouncedText(text);
+        },500);
+
+        return()=>{
+            clearTimeout(timerId);
+        };
+
+    },[text])
 
     useEffect(()=>{
 
@@ -15,7 +28,7 @@ const Convert=({language,text})=>{
 
             const {data}=await axios.post("https://translation.googleapis.com/language/translate/v2",{},{
             params:{
-                q:text,
+                q:debouncedText,
                 target:language.value,
                 key:"AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM"
             }
@@ -27,7 +40,7 @@ const Convert=({language,text})=>{
 
         doTranslation();              
 
-    },[language,text]);
+    },[language,debouncedText]);
 
     return (
     <div>
